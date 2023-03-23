aabfe507ea42ee1a8bb4616ab5b0748d46eb8c29
Jackson Yin <Yrh7383111@outlook.com>
2023-03-23 17:03:06 -0400

    Remove Element - Complete

---
+++ Remove Element/remove_element.py
@@ -0,0 +1,16 @@
+def removeElement(self, nums: list[int], val: int) -> int:
+    i = 0
+    end = len(nums) - 1
+
+    while i <= end:
+        if nums[i] == val:
+            if nums[end] == val:
+                end -= 1
+            else:
+                nums[i] = nums[end]
+                i += 1
+                end -= 1
+        else:
+            i += 1
+    
+    return i
