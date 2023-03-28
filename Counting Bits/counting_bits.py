c1be6e5548da6a840ec1c1107060acfaecda97cc
Jackson Yin <Yrh7383111@outlook.com>
2023-03-28 10:38:58 -0400

    Counting Bits - Update

--- Counting Bits/counting_bits.py
+++ Counting Bits/counting_bits.py
@@ -1,6 +1,5 @@
-class Solution:
-    def countBits(self, n: int) -> list[int]:
-        dp = [0] * (n + 1)
-        for i in range(1, n + 1):
-            dp[i] = dp[i // 2] + i % 2
-        return dp
+def countBits(n: int) -> list[int]:
+    dp = [0] * (n + 1)
+    for i in range(1, n + 1):
+        dp[i] = dp[i // 2] + i % 2
+    return dp
